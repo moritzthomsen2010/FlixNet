1f54b24774d1538b26a414fdbf92109612b44a38
Moritz Thomsen <moritz.thomsen@otto.de>
2025-11-20 11:13:06 +0100

    initial commit

---
+++ src/main/kotlin/com/gerontology/flixnet/FlixNetApplication.kt
@@ -0,0 +1,11 @@
+package com.gerontology.flixnet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlixNetApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlixNetApplication>(*args)
+}
---
+++ src/test/kotlin/com/gerontology/flixnet/FlixNetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gerontology.flixnet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlixNetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
